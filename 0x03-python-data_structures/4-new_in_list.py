e6a749c0e67166810a5587fbd32120e2226a82ae
ante-neh <amareanteneh12@gmail.com>
2022-06-04 05:28:41 -0400

    new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 4-new_in_list.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
+
+    © 2022 GitHub, Inc.
+
+    Terms
+    Privacy
+
